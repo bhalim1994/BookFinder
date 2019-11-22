1fc32bf08f9944f53f4730408907c1af943396be
Asher Gumunyu <ashergum@gmail.com>
2019-11-22 13:02:08 -0800

    Reading main posts script

---
+++ scripts/readPosts.js
@@ -0,0 +1,29 @@
+console.log("Getting Posts");
+var posts = [];
+var docRef = database.collection("GlobalPosts");
+docRef.onSnapshot(function(querySnapshot){
+    querySnapshot.forEach(function(doc){
+
+        createPost( doc.data().Title,
+                    doc.data().Price,
+                    doc.data().Description,)
+    })
+})
+
+
+
+
+//Body
+var body = document.getElementsByTagName("body");
+//Main container
+var container = document.getElementById("container");
+
+
+
+
+
+function createPost(title, price, description){
+    
+    
+
+}
