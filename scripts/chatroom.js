ea651d848545134de97f47db9ec75ae390e4945d
Ben Halim <bhalim1994@gmail.com>
2019-11-22 15:59:07 -0800

    Fixed formatting in chatroom.js

--- scripts/chatroom.js
+++ scripts/chatroom.js
@@ -1,3 +1,4 @@
 function add() {
-    document.getElementsByClassName('chat_s')[0].innerHTML += '<div class="chat_bubble-2">' + document.getElementsByClassName('chat_text')[0].value + '</div>'
+    document.getElementsByClassName('chat_s')[0].innerHTML += '<div class="chat_bubble-2">' +
+        document.getElementsByClassName('chat_text')[0].value + '</div>'
 }
