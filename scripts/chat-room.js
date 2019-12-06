ca7ff99a0cb351e828e7e0d4639ae5202c368fa2
Ben Halim <bhalim1994@gmail.com>
2019-12-06 08:45:41 -0800

    Fixed bugs with chat room and settings pages

--- scripts/chat-room.js
+++ scripts/chat-room.js
@@ -1,11 +1,11 @@
 //Add inputed text into chat message
 function add() {
-    document.getElementById('chatSent')[0].innerHTML += '<div class="chatBubble2">' +
-        document.getElementById('chatText')[0].value + '</div>';
+    document.getElementsByClassName('chatSent')[0].innerHTML += '<div class="chatBubble2">' +
+        document.getElementsByClassName('chatText')[0].value + '</div>';
 }
 
 // Clear text box after enter or button press
 window.onload = function clear () {
     var clear =
-        document.getElementById('chatText').value = '';
+        document.getElementsByClassName('chatText').value = '';
 }
