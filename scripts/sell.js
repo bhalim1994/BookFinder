b550662ddf4b7e700f068a40bcbda7499e29bf3e
ManpreetP <manpreet.parmar246@gmail.com>
2019-12-06 10:13:24 -0800

    Adding verified user restriction

--- scripts/sell.js
+++ scripts/sell.js
@@ -1,13 +1,5 @@
+/** stores where the image is uploaded to */
 var imageURL;
-var userUID;
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        // User is signed in.
-        userUID = user.uid;
-    } else {
-        // No user is signed in.
-    }
-});
 
 //Upload the file fo preview
 $(function () {
@@ -36,85 +28,82 @@ $(function () {
 //post button clicked
 $("#submit").closest('form').on('submit', function (event) {
     event.preventDefault();
-    // Check if content is empty
-    var title = document.getElementById("name").value;
-    var price = document.getElementById("price").value;
-    var university = document.getElementById("university").value;
-    var description = document.getElementById("description").value;
-    if(title == ""){
-        alert("Title is Empty.");
 
-    } else if (price == ""){
-        alert("There is no Price.");
+    // check if user is verified
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          if (user.emailVerified === false) {
+            alert("You have not verified your email.\nPlease verify by clicking on the link sent to your email.")
+            return;
+        } else {
+            // successful login 
+            // Check if content is empty
+            var title = document.getElementById("name").value;
+            var price = document.getElementById("price").value;
+            var university = document.getElementById("university").value;
+            var description = document.getElementById("description").value;
+            if(title == ""){
+                alert("Title is Empty.");
+            } else if (price == ""){
+                alert("There is no Price.");
+            } else if (university == "default"){
+                alert("Please choose your university");
+            } else if (imageURL == null){
+                alert("No image.");
+            } else { // Start post Upload
+                // Add a new document with a generated id.
+                database.collection("GlobalPosts").add({
+                    Title: title,
+                    Price: price,
+                    University: university,
+                    description: description,
+                    imageURL: "",
+                    PostOwner: user.email,
 
-    } else if (university == "default"){
-        alert("Please choose your university");
-
-    } else if (imageURL == null){
-        alert("No image.");
-
-    } else { // Start post Upload
-
-
-
-        // Add a new document with a generated id.
-        database.collection("GlobalPosts").add({
-            Title: title,
-            Price: price,
-            University: university,
-            description: description,
-            imageURL: "",
-            PostOwner: userUID,
-
-        })
-        .then(function(docRef) {
-
-            console.log("Document written with ID: ", docRef.id);
-            // Uploading image
-            var file = imageURL;
-            var storageRef = firebase.storage().ref();
-            var uploadTask = storageRef.child('GlobalPosts/' + docRef.id + "/postImage").put(file);
-            // Register three observers:
-            // 1. 'state_changed' observer, called any time the state changes
-            // 2. Error observer, called on failure
-            // 3. Completion observer, called on successful completion
-            uploadTask.on('state_changed', function(snapshot){
-                // Observe state change events such as progress, pause, and resume
-                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                console.log('Upload is ' + progress + '% done');
-                switch (snapshot.state) {
-                    case firebase.storage.TaskState.PAUSED: // or 'paused'
-                        console.log('Upload is paused');
-                        break;
-                    case firebase.storage.TaskState.RUNNING: // or 'running'
-                        console.log('Upload is running');
-                        break;
-                    }
-                }, function(error) {
-
-
-                    // Handle unsuccessful uploads
-                    console.log("upload failed");
-
-                }, function() {
-
-                    // Handle successful uploads on complete
-                    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-                    console.log('File available at', downloadURL);
-
-                    // Add image URL location
-                    database.collection("GlobalPosts").doc(docRef.id).update({
-                        imageURL: downloadURL,
-
-                    }).then(function () {
-                        console.log("URL successfully updated!");
-                        // go to success page
-                        window.location.href = "./sell-success.html";
-                });
-            });
-        })
-
-    })}
-    
+                }).then(function(docRef) {
+                    console.log("Document written with ID: ", docRef.id);
+                    // Uploading image
+                    var file = imageURL;
+                    var storageRef = firebase.storage().ref();
+                    var uploadTask = storageRef.child('GlobalPosts/' + docRef.id + "/postImage").put(file);
+                    // Register three observers:
+                    // 1. 'state_changed' observer, called any time the state changes
+                    // 2. Error observer, called on failure
+                    // 3. Completion observer, called on successful completion
+                    uploadTask.on('state_changed', function(snapshot){
+                        // Observe state change events such as progress, pause, and resume
+                        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log('Upload is ' + progress + '% done');
+                        switch (snapshot.state) {
+                            case firebase.storage.TaskState.PAUSED: // or 'paused'
+                                console.log('Upload is paused');
+                                break;
+                            case firebase.storage.TaskState.RUNNING: // or 'running'
+                                console.log('Upload is running');
+                               break;
+                            }
+                        }, function(error) {
+                            // Handle unsuccessful uploads
+                            console.log("upload failed");
+                        }, function() {
+                            // Handle successful uploads on complete
+                            uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                                console.log('File available at', downloadURL);
+                                // Add image URL location
+                                database.collection("GlobalPosts").doc(docRef.id).update({
+                                imageURL: downloadURL,
+                            }).then(function () {
+                                console.log("URL successfully updated!");
+                                // go to success page
+                                window.location.href = "./sell-success.html";
+                            });
+                        });
+                    })
+                })}
+            }
+        } else {
+            //User not logged in
+        }
+    });
 });
