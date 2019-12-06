d03c61ea029fb44dbafb7edba5317a489a174f5a
Asher Gumunyu <ashergum@gmail.com>
2019-12-06 10:32:00 -0800

    formating

--- scripts/firebase-auth.js
+++ scripts/firebase-auth.js
@@ -5,25 +5,22 @@ firebase.auth().onAuthStateChanged(function(user) {
       console.log("user is logged in");
       if(user.emailVerified === true){
         	// Add user Profile to database
-	database.collection("Users").doc(user.uid).update({
-    isVerified: true,
-    //Show profile updated
-  }).then(function () {
-    console.log("user is verified");
-  })
+	        database.collection("Users").doc(user.uid).update({
+              isVerified: true,
+              //Show profile updated
+          }).then(function () {
+                console.log("user is verified");
 
-  .catch(function (error) {
-    // The document probably doesn't exist.
-    alert("Error updating Profile");
-    console.error("Error updating Profile", error);
-  });
-      }
-
-    } else {
-      // User is signed out.
-	  console.log("user is logged out");
-	  window.location.href = "./login.html";
-      // Do nothing
-      
+      }).catch(function (error) {
+            // The document probably doesn't exist.
+            alert("Error updating Profile");
+            console.error("Error updating Profile", error);
+      });
     }
-  });
+  } else {
+    // User is signed out.
+    console.log("user is logged out");
+    window.location.href = "./login.html";
+    // Do nothing
+  }
+});
