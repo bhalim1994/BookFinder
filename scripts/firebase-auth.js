454c2d69cd07eb2ba84de366fac8113ce1e69e5f
ManpreetP <manpreet.parmar246@gmail.com>
2019-12-06 10:24:39 -0800

    verified changer

--- scripts/firebase-auth.js
+++ scripts/firebase-auth.js
@@ -3,6 +3,21 @@ firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
       console.log("user is logged in");
+      if(user.emailVerified === true){
+        var docRef = database.collection("Users").doc(user.uid);
+        docRef.get().then(function (doc) {
+          if (doc.exists) {
+            console.log("Document data:", doc.data());
+            doc.data().isVerified = true;
+            console.log("user is verified");
+          } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+          }
+        }).catch(function (error) {
+          console.log("Error getting document:", error);
+        });
+      }
 
     } else {
       // User is signed out.
