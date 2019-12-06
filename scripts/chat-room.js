26b9f0640f26e5c6664b3227c9429f083809b093
Ben Halim <bhalim1994@gmail.com>
2019-12-06 06:18:58 -0800

    Content class updated to ID

--- scripts/chat-room.js
+++ scripts/chat-room.js
@@ -1,11 +1,11 @@
 //Add inputed text into chat message
 function add() {
-    document.getElementsByClassName('chat_s')[0].innerHTML += '<div class="chat_bubble-2">' +
-        document.getElementsByClassName('chatText')[0].value + '</div>';
+    document.getElementById('chatSent')[0].innerHTML += '<div class="chatBubble2">' +
+        document.getElementById('chatText')[0].value + '</div>';
 }
 
 // Clear text box after enter or button press
 window.onload = function clear () {
     var clear =
-        document.getElementsByClassName('chatText').value = '';
+        document.getElementById('chatText').value = '';
 }
